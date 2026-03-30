1e43785bb73a8278b0ff17e0f0898961163f9375
Naman <naman1159.be25@chitkarauniversity.edu.in>
2026-03-30 22:37:59 +0530

    modify temperature ,wind speed,humidity using js

---
+++ app.js
@@ -0,0 +1,19 @@
+async function fetchApi(city){
+    url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=73cd1bf3925ef1f476668800e0a88ef1&units=metric`);
+    let data = await url.json();
+    return `${Math.ceil(data.main.temp)},${Math.ceil(data.wind.speed)},${data.main.humidity}`;
+}
+let inputBtn = document.body.querySelector("#cityInput");
+let searchBtn = document.body.querySelector("#searchBtn");
+let temp = document.body.querySelector(".weather .temp");
+let humidity = document.body.querySelector(".humidity");
+let speed = document.body.querySelector(".wind");
+let arr = [];
+searchBtn.addEventListener("click",async ()=>{
+    let city = inputBtn.value;
+    let details = await fetchApi(city);
+    arr = details.split(",");
+    temp.innerHTML = `${arr[0]}°C`;
+    speed.innerHTML = `${arr[1]}Km/h`;
+    humidity.innerHTML = `${arr[[2]]}%`;
+});
