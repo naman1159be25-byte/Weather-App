609dc53fb94da7a0771e8635345495f19bd781b2
Naman <naman1159.be25@chitkarauniversity.edu.in>
2026-03-31 10:06:38 +0530

    add modify details function to change details

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
+async function modifyDetails(){
+    let city = inputBtn.value;
+    let details = await fetchApi(city);
+    arr = details.split(",");
+    console.log(arr);
+    temp.innerHTML = `${arr[0]}°C`;
+    speed.innerHTML = `${arr[1]}Km/h`;
+    humidity.innerHTML = `${arr[[2]]}%`;
+    City.innerHTML = `${city.toUpperCase()}`;
+}
 async function fetchApi(city){
-    url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=73cd1bf3925ef1f476668800e0a88ef1&units=metric`);
+    let url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=73cd1bf3925ef1f476668800e0a88ef1&units=metric`);
     let data = await url.json();
     return `${Math.ceil(data.main.temp)},${Math.ceil(data.wind.speed)},${data.main.humidity}`;
 }
@@ -8,12 +18,9 @@ let searchBtn = document.body.querySelector("#searchBtn");
 let temp = document.body.querySelector(".weather .temp");
 let humidity = document.body.querySelector(".humidity");
 let speed = document.body.querySelector(".wind");
+let City = document.body.querySelector(".city");
 let arr = [];
 searchBtn.addEventListener("click",async ()=>{
-    let city = inputBtn.value;
-    let details = await fetchApi(city);
-    arr = details.split(",");
-    temp.innerHTML = `${arr[0]}°C`;
-    speed.innerHTML = `${arr[1]}Km/h`;
-    humidity.innerHTML = `${arr[[2]]}%`;
+    modifyDetails();
 });
+
