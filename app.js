56527dd98ec51ed5374ed28ef7163f8781b68554
Naman <naman1159.be25@chitkarauniversity.edu.in>
2026-03-31 13:28:00 +0530

    add functionality by pressing enter

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ async function modifyDetails(){
     let city = inputBtn.value;
     let details = await fetchApi(city);
     arr = details.split(",");
-    console.log(arr);
     temp.innerHTML = `${arr[0]}°C`;
     speed.innerHTML = `${arr[1]}Km/h`;
     humidity.innerHTML = `${arr[[2]]}%`;
@@ -11,6 +10,7 @@ async function modifyDetails(){
 async function fetchApi(city){
     let url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=73cd1bf3925ef1f476668800e0a88ef1&units=metric`);
     let data = await url.json();
+    console.log(data);
     return `${Math.ceil(data.main.temp)},${Math.ceil(data.wind.speed)},${data.main.humidity}`;
 }
 let inputBtn = document.body.querySelector("#cityInput");
@@ -23,4 +23,9 @@ let arr = [];
 searchBtn.addEventListener("click",async ()=>{
     modifyDetails();
 });
+document.body.addEventListener("keydown",(ele)=>{
+    if(inputBtn.value!="" && ele.key.toLowerCase()=="enter" ){
+        modifyDetails();
+    }
+})
 
