84a90218c7a4c87746620f2aeb35d9c61eab0264
Naman <naman1159.be25@chitkarauniversity.edu.in>
2026-04-02 22:32:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ async function modifyDetails(){
     changeIcon(arr[3]);
 }
 async function fetchApi(city){
-    let url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=73cd1bf3925ef1f476668800e0a88ef1&units=metric`);
+    let url = await fetch(`YOUR AP KEY`);
     let data = await url.json();
     return `${Math.ceil(data.main.temp)},${Math.ceil(data.wind.speed)},${data.main.humidity},${data.weather[0].main}`;
 }
